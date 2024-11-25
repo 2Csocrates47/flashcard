f6e4b87a89c8b33f333356e45550873287630e49
2Csocrates47 <00001118529145sp@al.educacao.sp.gov.br>
2024-11-24 23:27:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'esporte',
+    'O que é saque?',
+    'O saque no vôlei é o ato de enviar a bola da área de saque para a quadra adversária. Essa ação é feita pelo jogador que ocupa a posição 1, golpeando a bola solta no ar com a mão ou parte do braço.'
+)
+
+criaCartao(
+    'filosofia',
+    'Quem foi socrates?',
+    'Sócrates foi um grego de origem humilde que revolucionou o pensamento ocidental de tal modo que foi condenado à morte em 399 a.C.'
+)
+
+criaCartao(
+    'esportes',
+    'O que é um toque no volei?',
+    'Toque é uma técnica do voleibol feita tocando a bola com as duas mãos ao mesmo tempo, sobre a cabeça e usando os dedos. É a principal técnica usada para fazer um levantamento'
+)
+
+criaCartao(
+    'filosofia',
+    'quem foi Friedrich Nietzsche?',
+    'Friedrich Nietzsche (1844-1900) foi um filósofo, poeta, músico e ensaísta alemão, uma das figuras mais influentes da filosofia ocidental. Seu pensamento teve um impacto profundo na filosofia, na psicologia, nas ciências sociais e nas artes, sendo frequentemente associado a temas como a moral, a verdade, o poder e a liberdade individual.'
+)
